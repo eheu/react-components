29eb053b3ac47cb9c559c4f3bdafbdb8995e82d3
Emil Heunecke <heunecke@outlook.com>
2020-06-11 19:07:55 +0200

    checkpoint

--- src/main.js
+++ src/main.js
@@ -2,6 +2,4 @@ import React from "react";
 import { render } from "react-dom";
 import { ReactCounter } from "./components";
 
-const app = <ReactCounter />
-
-render(app, document.querySelector("#root"));
+render(<ReactCounter />, document.querySelector("#root"));
